126fda67e3617f23e14c376802ed4521c0fcfd8b
Hemanth2001 <hhemanth660@gmail.com>
2022-12-04 07:47:06 -0500

    learn

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,7 +8,8 @@ import 'package:intl/intl.dart';
 class TransactionList extends StatefulWidget {
   final List<Transaction> transactions;
   final Function deleteTx;
-TransactionList(this.transactions,this.deleteTx);
+
+  TransactionList(this.transactions, this.deleteTx);
 
   @override
   State<TransactionList> createState() => _TransactionListState();
@@ -17,47 +18,48 @@ TransactionList(this.transactions,this.deleteTx);
 class _TransactionListState extends State<TransactionList> {
   @override
   Widget build(BuildContext context) {
-    return widget.transactions.isEmpty?Column(
-        children: [
-          Text("No Transactions are not add yet"),
-          Container(
-              height:MediaQuery.of(context).size.height*0.2,
-              child: Image.asset('assets/images/f.jpg', fit: BoxFit.cover))
-        ],
-      )
-
-
-    :ListView.builder(
-      itemCount: widget.transactions.length ,
-      itemBuilder: (ctx , index){
-      return Card(
-        child:
-            ListTile(
-            leading:CircleAvatar(radius: 40,
+    return widget.transactions.isEmpty
+        ? Column(
+            children: [
+              Text("No Transactions are not add yet"),
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: Image.asset('assets/images/f.jpg', fit: BoxFit.cover))
+            ],
+          )
+        : ListView.builder(
+            itemCount: widget.transactions.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 40,
                     child: FittedBox(
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          '\$' + widget.transactions[index].amount.toStringAsFixed(2),
-                          style: TextStyle(fontSize: 18,fontWeight: FontWeight
-                              .bold),
+                          '\$' +
+                              widget.transactions[index].amount
+                                  .toStringAsFixed(2),
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
                         ),
                       ),
                     ),
                   ),
-              title:
-                    Text(
-                      widget.transactions[index].title,
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-
-                 subtitle:   Text(DateFormat().format(widget.transactions[index]
-                     .date)
-                    ),
-              trailing:IconButton(onPressed: ()=>widget.deleteTx(widget.transactions[index].id),
-                  icon: Icon(Icons.delete),) ,
-            ),
-    );
-    });
+                  title: Text(
+                    widget.transactions[index].title,
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  subtitle: Text(
+                      DateFormat().format(widget.transactions[index].date)),
+                  trailing: IconButton(
+                    onPressed: () =>
+                        widget.deleteTx(widget.transactions[index].id),
+                    icon: Icon(Icons.delete),
+                  ),
+                ),
+              );
+            });
   }
 }
